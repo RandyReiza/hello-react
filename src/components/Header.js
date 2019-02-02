6ac80161160389e0bdd121f2f22dbe2360cdecfa
Randy Reiza <randy.aries.95@gmail.com>
2019-02-02 11:59:59 +0700

    Handling Events pada Class Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
   constructor(props){
     super(props);
-    this.state = {                              // state sama fungsinya seperti var {dapat menampung value}, namun state dapat merubah value secara dinamis, sehingga saat value diubah tidak perlu refresh halaman web
+    this.state = {                                      // state sama fungsinya seperti var {dapat menampung value}, namun state dapat merubah value secara dinamis, sehingga saat value diubah tidak perlu refresh halaman web
       daftar: "Daftar Makanan Nusantara",
       dataList: this.props.list
     };
+    this.handlePesan = this.handlePesan.bind(this);     // ini supaya bisa menggunakan 'this' didalam fungsi, seperti fungsi 'handlePesan' dibawah
+  }
+
+  handlePesan() {
+    // alert("Halaman Header");
+    alert(this.state.daftar);
   }
 
   render() {
@@ -15,6 +21,7 @@ class Header extends Component {
         <h2>Makanan khas Indonesia</h2>
         <p>{this.state.daftar}</p>
         <p>{this.state.dataList}</p>
+        <a href="/" onClick={this.handlePesan}>Halaman Header</a>
       </div>
     )
   }
