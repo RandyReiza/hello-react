1559ebfe5f3803a619be891b4ae6360b7f643ce6
Randy Reiza <randy.aries.95@gmail.com>
2019-02-02 11:04:42 +0700

    Penulisan State

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state = {                                          // state sama fungsinya seperti var {dapat menampung value},
+      daftar: "Daftar Makanan Nusantara"                    // namun state dapat merubah value secara dinamis,
+    };                                                      // sehingga saat value diubah tidak perlu refresh halaman web
+  }
+
   render() {
     return (
       <div>
         <h2>Makanan khas Indonesia</h2>
+        <p>{this.state.daftar}</p>
       </div>
     )
   }
