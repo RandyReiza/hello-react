9f555c225e5631d51af35a968f8ddf307d0664a4
Randy Reiza <randy.aries.95@gmail.com>
2019-02-02 09:49:48 +0700

    Props pada function component

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div>
         <Header />
         <List />
-        <Footer />
+        <Footer name="Makanan Nusantara" tahun="1990"/>
       </div>
     );
   }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const Footer = () => {
-  var name = "NIOMIC";
+const Footer = props => {
   return (
     <div>
-      <h4>Halaman Footer {name}</h4>
-      <p>@2018</p>
+      <h4>Halaman Footer {props.name}</h4>
+      <p>@{props.tahun}</p>
     </div>
   );
 };
