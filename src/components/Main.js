e6468bcee10dc514b8682a514b66caf1fc54251a
Randy Reiza <randy.aries.95@gmail.com>
2019-02-04 19:18:50 +0700

    Multiple State

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,18 +5,18 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "Menu Makanan"
+      title: "Menu Makanan",
+      title2: "Menu Minuman"
     };
-    this.rubahData = this.rubahData.bind(this);               // utk dapat memasukkan 'this' ke dalam fungsi yg dituju, fungsi yg dituju dlm kasus ini adalah fungsi 'rubahData'
+    this.rubahData = this.rubahData.bind(this);
   }
 
-  // rubahData() {
-  //   this.setState({title: "Pilih Makanan"});
-  // }
-
-  rubahData() {                                               // bentuk lain utk fungsi 'rubahData', utk merubah value state
-    this.setState((state, props) => {                         // dan ini penulisan yg RECOMENDED
-      return {title: "Pilih Menu Makanan"};
+  rubahData() {
+    this.setState((state, prop) => {                          // 'state' & 'props' dalam parameter, dpt dihilangkan. namun jika ingin memasukkan state atau props utk fungsi tsb, maka hrs dimasukkan ke dlm parameter
+      return {
+        title: state.title2,                                  // seperti contoh ini, state.title1 ingin diisi oleh state.title2, jd di parameter dimasukin 'state'
+        title2: state.title
+      };
     });
   }
 
@@ -24,6 +24,7 @@ class Main extends Component {
     return(
       <div>
         <h3>{this.state.title}</h3>
+        <h2>{this.state.title2}</h2>
         <button onClick={this.rubahData}>Rubah Data</button>
       </div>
     );
